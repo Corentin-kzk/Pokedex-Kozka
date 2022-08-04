d47aad445f057fd462fd9c55590a7d3cfdcebad6
corentin-kzk <corentin@polydea.eu>
2022-08-04 08:27:48 +0200

    add modal to delete

--- src/Components/cardPokedex/Card.jsx
+++ src/Components/cardPokedex/Card.jsx
@@ -8,6 +8,9 @@ import "https://kit.fontawesome.com/b99e675b6e.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
+import Modal from "../Modal/Modal";
+import useModal from "../Modal/useModal";
+import Button from "../button/Button";
 
 const getTheId = (url) => {
   let splitedUrl = url.split("/").map((element) => element.trim());
@@ -15,6 +18,7 @@ const getTheId = (url) => {
 };
 
 const Card = ({ pokemon }) => {
+  const { isShowing: Showed, toggle: toggle } = useModal();
   const pokedex = useSelector((state) => state.pokedex);
 
   const dispatch = useDispatch();
@@ -22,7 +26,7 @@ const Card = ({ pokemon }) => {
 
   const savedInPokedex = () => {
     if (isPresent) {
-      dispatch(remove(pokemon));
+      toggle();
     } else {
       dispatch(add(pokemon));
     }
@@ -30,29 +34,49 @@ const Card = ({ pokemon }) => {
   const id = getTheId(pokemon.url);
 
   return (
-    <div className="card">
-      <h2 className="card-title">{pokemon.name}</h2>
-      <img
-        className="image"
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-        alt="Pokemon Image"
-      />
-      <Link className="detail-btn" to={`/pokemon/${id}`}>
-        <div className="detail-div">
-          <img className="button-more" src={pokeBall} alt="Details Button" />
-          <span>Details</span>
-        </div>
-      </Link>
-      <div className="pokedex-bookmark">
-        <FontAwesomeIcon
-          className={isPresent ? "active" : ""}
-          icon={faBookmark}
-          onClick={() => {
-            savedInPokedex();
+    <>
+      <Modal isShowing={Showed} hide={toggle} title="Confirm Delete">
+        <p style={{ lineHeight: 5, letterSpacing: "2px" }}>
+          Are you sure you want release{" "}
+          <span style={{ fontWeight: "bolder", color: "red" }}>
+            {pokemon.name}
+          </span>
+          ?{" "}
+        </p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
           }}
+        >
+          <Button name={"YES"} onClick={() => dispatch(remove(pokemon))} />
+          <Button name={"NO"} onClick={toggle} />
+        </div>
+      </Modal>
+      <div className="card">
+        <h2 className="card-title">{pokemon.name}</h2>
+        <img
+          className="image"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+          alt="Pokemon Image"
         />
+        <Link className="detail-btn" to={`/pokemon/${id}`}>
+          <div className="detail-div">
+            <img className="button-more" src={pokeBall} alt="Details Button" />
+            <span>Details</span>
+          </div>
+        </Link>
+        <div className="pokedex-bookmark">
+          <FontAwesomeIcon
+            className={isPresent ? "active" : ""}
+            icon={faBookmark}
+            onClick={() => {
+              savedInPokedex();
+            }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
